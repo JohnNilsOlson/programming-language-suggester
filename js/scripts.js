d35c738c97a3df23fc3fceb79bb20effcf858284
JohnNils Olson <johnnils@gmail.com>
2020-06-05 10:36:27 -0700

    reformat html, add color form, add script for color form, light css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   $("#name").submit(function(event) {
+    event.preventDefault();
     const firstName = $("input#firstname").val();
     const lastName = $("input#lastname").val();
+    $("#hidden1").fadeIn(500);
+    console.log(firstName);
+    console.log(lastName);
+  });
 
-    console.log(firstName, lastName);
-
+  $("#color").submit(function(event) {
     event.preventDefault();
+    const color = $("#color").val();
+    $("hidden2").fadeIn(500);
+    console.log(color);
   });
 });
