6653f3350096e4e49a0ae427afa106eb09881b70
JohnNils Olson <johnnils@gmail.com>
2020-06-05 13:08:09 -0700

    clean up html

--- js/scripts.js
+++ js/scripts.js
@@ -10,35 +10,35 @@ $(document).ready(function() {
 
   $("#next2").click(function(event) {
     event.preventDefault();
-    const color = $("select#color").val();
+    const color = parseInt($("select#color").val());
     $("#q2").fadeIn(500);
     console.log(color);
   });
 
   $("#next3").click(function(event) {
     event.preventDefault();
-    const animal = $("select#animal").val();
+    const animal = parseInt($("select#animal").val());
     $("#q3").fadeIn(500);
     console.log(animal);
   });
 
   $("#next4").click(function(event) {
     event.preventDefault();
-    const food = $("select#food").val();
+    const food = parseInt($("select#food").val());
     $("#q4").fadeIn(500);
     console.log(food);
   });
 
   $("#next5").click(function(event) {
     event.preventDefault();
-    const climate = $("select#climate").val();
+    const climate = parseInt($("select#climate").val());
     $("#q5").fadeIn(500);
     console.log(climate);
   });
 
   $("#submit").submit(function(event) {
     event.preventDefault();
-    const literature = $("select#literature").val();
+    const literature = parseInt($("select#literature").val());
     console.log(literature);
   });
 });
