7c919dbddf134c48c84bfcce6b8d478cb4f3c0f4
JohnNils Olson <johnnils@gmail.com>
2020-06-05 14:28:57 -0700

    remove forms within forms, clean up html, refactor scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,47 @@
 $(document).ready(function() {
-  $("#next1").click(function(event) {
-    event.preventDefault();
+  $("#survey").submit(function(event) {
     const firstName = $("input#firstname").val();
     const lastName = $("input#lastname").val();
+    const color = parseInt($("select#color").val());
+    const animal = parseInt($("select#animal").val());
+    const food = parseInt($("select#food").val())
+    const climate = parseInt($("select#climate").val());
+    const literature = parseInt($("select#literature").val());
+
+    if ((color + animal + food + climate + literature) >= 0) {
+      $("#name-return").text(firstName);
+    }
+
+  });  
+
+  $("#next1").click(function(event) {
+    event.preventDefault();
     $("#q1").fadeIn(500);
-    console.log(firstName);
-    console.log(lastName);
   });
 
   $("#next2").click(function(event) {
     event.preventDefault();
-    const color = parseInt($("select#color").val());
     $("#q2").fadeIn(500);
-    console.log(color);
   });
 
   $("#next3").click(function(event) {
     event.preventDefault();
-    const animal = parseInt($("select#animal").val());
     $("#q3").fadeIn(500);
-    console.log(animal);
   });
 
   $("#next4").click(function(event) {
     event.preventDefault();
-    const food = parseInt($("select#food").val());
     $("#q4").fadeIn(500);
-    console.log(food);
   });
 
   $("#next5").click(function(event) {
     event.preventDefault();
-    const climate = parseInt($("select#climate").val());
     $("#q5").fadeIn(500);
-    console.log(climate);
   });
 
-  $("#submit").submit(function(event) {
+  $("#next6").click(function(event) {
     event.preventDefault();
-    const literature = parseInt($("select#literature").val());
-    console.log(literature);
-  });
+    $("#q6").fadeIn(500);
+  }); 
+
 });
